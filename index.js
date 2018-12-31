7b5811fc26135a21c8b5f5d2c6fc160b57e3bc96
Chris Marfia <marfiadocs@gmail.com>
2018-12-31 14:54:10 -0600

    fix text bug

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ void function(Elm) {
   }
 
   var synth = window.speechSynthesis;
-  var voice = synth.getVoices()[0];
   app.ports.speak.subscribe(function(text) {
     document.getElementById('voices').innerText = JSON.stringify(window.speechSynthesis.getVoices().map(function(voice) { return voice.name; }), null, '\t');
     var utterance = new SpeechSynthesisUtterance(text);
-    utterance.voice = voice;
+    utterance.voice = synth.getVoices()[0];
     //utterance.pitch = 1;
     //utterance.rate = 1;
     synth.speak(utterance);
